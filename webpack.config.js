8ab702b173c37ae9ecd5d3233c69d211c6384b72
Dave-Sheridan <shero.dave@gmail.com>
2023-07-16 14:36:56 -0500

    setup changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,3 +4,15 @@ const webpack = require('webpack')
 module.exports = {
     entry: './src/client/index.js'
 }
+
+module: {
+    rules: [
+            {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+            }
+    ]
+}
+
+output: { ...output options }
